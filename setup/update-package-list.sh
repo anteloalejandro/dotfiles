d78f2735aa8f00eb3b1bfda7cb7cf78fb19d9d12
Alejandro Antelo <alfasant@protonmail.com>
2024-07-26 11:10:33 +0200

    update package list

--- setup/update-package-list.sh
+++ setup/update-package-list.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname $0)
 
-pacman -Qeqn | grep -Ev 'steam' > native-packages.txt
+pacman -Qeqn | grep -Ev 'steam|lutris|mangohud' > native-packages.txt
 pacman -Qeqm | grep -Ev 'yay|sm64|autofirma' > external-packages.txt
