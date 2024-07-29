c500a4db3170fe4acf89e6adde14366fcb0671e4
Alejandro Antelo <alfasant@protonmail.com>
2024-07-29 16:34:26 +0200

    add kanata remapper

--- setup/enable-services.sh
+++ setup/enable-services.sh
@@ -4,6 +4,7 @@ sudo systemctl enable ly                   # login
 sudo systemctl enable nvidia-persistenced  # Hybrid graphics
 sudo systemctl enable pipewire-pulse       # Pulseudio compat.
 sudo systemctl enable bluetooth            # bluetooth
+sudo systemctl enable kanata               # keyboard remapper
 
 sudo systemctl disable iwd                 # wifi
 sudo systemctl enable NetworkManager       #
