bb0c264f3aeb97a5770d667eebce174acaf7c13f
Alejandro Antelo <alfasant@protonmail.com>
2025-05-01 22:51:49 +0200

    add osd and notifications with shared colors

---
+++ .config/.styles/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+mkdir -p 'generated'
+
+rm 'generated/style.hyprland.conf'
+rm 'generated/style.scss'
+
+while read line; do
+  name=$(echo $line | cut -d':' -f1 | tr -d ' ')
+  value=$(echo $line | cut -d':' -f2 | tr -d ' ')
+
+  echo "\$$name = rgba($value)" >> 'generated/style.hyprland.conf'
+  echo "\$$name: #$value;" >> 'generated/style.scss'
+done < 'colors'
+
+while read line; do
+  name=$(echo $line | cut -d':' -f1 | tr -d ' ')
+  value=$(echo $line | cut -d':' -f2 | tr -d ' ')
+
+  echo "\$$name = $value" >> 'generated/style.hyprland.conf'
+  echo "\$$name: $value;" >> 'generated/style.scss'
+done < 'variables'
+
+hyprctl reload
+if test -n "$(ags list | grep astal)"; then
+  ags quit
+  ags run &
+fi
--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -2,9 +2,31 @@ import { App } from "astal/gtk3"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
 
+import osd_style from "./osd/OSD.scss"
+import OSD from "./osd/OSD"
+
+import notification_style from "./notifications/Notification.scss"
+import NotificationPopups from "./notifications/NotificationPopups"
+
 App.start({
     css: style,
     main() {
         App.get_monitors().map(Bar)
     },
+})
+
+App.start({
+    instanceName: "osd",
+    css: osd_style,
+    main() {
+        App.get_monitors().map(OSD)
+    },
+})
+
+App.start({
+    instanceName: "notification_popups",
+    css: notification_style,
+    main() {
+        App.get_monitors().map(NotificationPopups)
+    }
 })
