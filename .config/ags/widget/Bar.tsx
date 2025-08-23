1752f2bbaeef7a699cfe1a53fd9f8e7e821f68fd
Alejandro Antelo <alfasant@protonmail.com>
2025-08-23 19:43:46 +0200

    add dinamically sized corner

--- .config/ags/widget/Bar.tsx
+++ .config/ags/widget/Bar.tsx
@@ -2,12 +2,17 @@ import app from "ags/gtk4/app"
 import { Astal, Gtk, Gdk } from "ags/gtk4"
 import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
-import { RoundedCorner, CornerOrientation } from "./corners"
+import { RoundedCorner, CornerOrientation, DynRoundedCorner } from "./corners"
+import { createState } from "gnim"
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-  const time = createPoll("", 1000, "date")
   const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
   const { TOP_LEFT, TOP_RIGHT } = CornerOrientation;
+
+  const [ hspace, setHspace ] = createState(20);
+  const [ vspace, setVspace ] = createState(20);
+
+  let topBarOpened = false;
 
   return (
     <window
@@ -19,17 +24,39 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       anchor={TOP | LEFT | RIGHT}
       application={app}
     >
-      <centerbox cssName="centerbox">
-        <box $type="start">
-          <RoundedCorner padding={20} radius={8} orientation={TOP_LEFT} />
-        </box>
-        <box $type="center" class="border-top-bar" hexpand vexpand>
-          <label>Hello world</label>
-        </box>
-        <box $type="end">
-          <RoundedCorner padding={20} radius={8} orientation={TOP_RIGHT} />
-        </box>
-      </centerbox>
+      <button cssName="eventbox"
+
+        onClicked={_ => {
+          if (topBarOpened) {
+            setVspace(v => v-10);
+            console.log("unfocused");
+          } else {
+            setVspace(v => v+10);
+            console.log("focused");
+          }
+
+          topBarOpened = !topBarOpened;
+        }}
+      >
+        <centerbox cssName="centerbox">
+          <box $type="start">
+            <DynRoundedCorner vspace={vspace} hspace={hspace} radius={8} orientation={TOP_LEFT} />
+          </box>
+          <box $type="center" class="border-top-bar" hexpand vexpand valign={Gtk.Align.START}
+            height_request={vspace.get() + 8}
+            $={self => {
+              vspace.subscribe(() => {
+                self.set_size_request(self.get_size_request()[0], vspace.get() + 8)
+              })
+            }}
+          >
+            <label>Hello world</label>
+          </box>
+          <box $type="end">
+            <DynRoundedCorner vspace={vspace} hspace={hspace} radius={8} orientation={TOP_RIGHT} />
+          </box>
+        </centerbox>
+      </button>
     </window>
   )
 }
--- .config/ags/widget/corners.tsx
+++ .config/ags/widget/corners.tsx
@@ -1,5 +1,6 @@
 import { Gdk, Gtk } from "ags/gtk4";
 import cairo from "gi://cairo?version=1.0";
+import { Accessor } from "gnim";
 
 /** Orientation of the corner relative to the drawing area */
 export enum CornerOrientation {
@@ -22,6 +23,8 @@ export type RoundedCornerProps = {
 export function RoundedCorner(props: RoundedCornerProps) {
   const r = props.radius; // corner radius
   const padding = props.padding ?? 0;
+  const size = r + padding;
+
   let drawing = new Gtk.DrawingArea({
     cssName: "corner",
     hexpand: false, vexpand: false,
@@ -30,7 +33,7 @@ export function RoundedCorner(props: RoundedCornerProps) {
   c.parse("#181818");
 
   drawing.set_draw_func((area, cr, _width, _height) => {
-    area.set_size_request(r + padding, r + padding);
+    area.set_size_request(size, size);
     cr.scale(1,1);
 
     // clear for transparent background
@@ -44,32 +47,32 @@ export function RoundedCorner(props: RoundedCornerProps) {
     
     switch (props.orientation) {
       case CornerOrientation.TOP_LEFT:
-        cr.moveTo(0, r + padding);
-        cr.lineTo(padding, r + padding);
-        cr.arc(r + padding, r + padding, r, Math.PI, (3 * Math.PI)/2);
-        cr.lineTo(r + padding, 0);
+        cr.moveTo(0, size);
+        cr.lineTo(padding, size);
+        cr.arc(size, size, r, Math.PI, (3 * Math.PI)/2);
+        cr.lineTo(size, 0);
         cr.lineTo(0, 0);
         break;
       case CornerOrientation.TOP_RIGHT:
         cr.moveTo(0, 0);
         cr.lineTo(0, padding);
-        cr.arc(0, r + padding, r, (3 * Math.PI)/2, Math.PI * 2);
-        cr.lineTo(r + padding, r + padding);
-        cr.lineTo(r + padding, 0);
+        cr.arc(0, size, r, (3 * Math.PI)/2, Math.PI * 2);
+        cr.lineTo(size, size);
+        cr.lineTo(size, 0);
         break;
       case CornerOrientation.BOTTOM_LEFT:
-        cr.moveTo(r + padding, r + padding);
-        cr.lineTo(r + padding, r);
-        cr.arc(r + padding, 0, r, (3 * Math.PI)/2, Math.PI);
+        cr.moveTo(size, size);
+        cr.lineTo(size, r);
+        cr.arc(size, 0, r, (3 * Math.PI)/2, Math.PI);
         cr.lineTo(0, 0);
-        cr.lineTo(0, r + padding);
+        cr.lineTo(0, size);
         break;
       case CornerOrientation.BOTTOM_RIGHT:
-        cr.moveTo(r + padding, 0);
+        cr.moveTo(size, 0);
         cr.lineTo(r, 0);
         cr.arc(0, 0, r, Math.PI*2, Math.PI/2);
-        cr.lineTo(0, r + padding);
-        cr.lineTo(r + padding, r + padding);
+        cr.lineTo(0, size);
+        cr.lineTo(size, size);
         break;
     }
     cr.closePath();
@@ -78,4 +81,45 @@ export function RoundedCorner(props: RoundedCornerProps) {
   })
 
   return drawing;
+}
+
+export function DynRoundedCorner(props: RoundedCornerProps & {vspace: Accessor<number>, hspace: Accessor<number>}) {
+  const { HORIZONTAL, VERTICAL } = Gtk.Orientation;
+  const { TOP_LEFT, BOTTOM_LEFT, BOTTOM_RIGHT, TOP_RIGHT } = CornerOrientation;
+  const { vspace, hspace, radius, orientation } = props;
+  props.padding = undefined;
+
+  const setup_hSpacer = (self: Gtk.Box) => {
+    self.add_css_class("h-spacer");
+    self.set_valign(Gtk.Align.START);
+    self.set_size_request(hspace.get(), vspace.get()+radius);
+    hspace.subscribe(() => {
+      self.set_size_request(hspace.get(), self.get_size_request()[1]);
+    })
+    vspace.subscribe(() => {
+      self.set_size_request(self.get_size_request()[0], vspace.get()+radius);
+    })
+  }
+  const setup_vSpacer = (self: Gtk.Box) => {
+    self.add_css_class("v-spacer");
+    self.set_size_request(self.get_size_request()[0], vspace.get());
+    vspace.subscribe(() => {
+      self.set_size_request(self.get_size_request()[0], vspace.get());
+    })
+  }
+
+  const isTop = orientation == TOP_LEFT || orientation == TOP_RIGHT;
+  const isLeft = orientation == TOP_LEFT || orientation == BOTTOM_LEFT;
+
+  return (
+    <box orientation={HORIZONTAL}>
+      { isLeft && <box $={setup_hSpacer} /> }
+      <box orientation={VERTICAL}>
+        { isTop && <box $={setup_vSpacer} /> }
+        <RoundedCorner {...props} />
+        { !isTop && <box $={setup_vSpacer} /> }
+      </box>
+      { !isLeft && <box $={setup_hSpacer} /> }
+    </box>
+  )
 }
