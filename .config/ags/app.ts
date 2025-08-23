2df205980a11f7bb77b55e582308e081b87a1436
Alejandro Antelo <alfasant@protonmail.com>
2025-08-23 12:15:46 +0200

    clean ags template

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,73 +1,10 @@
-import { App, Widget } from "astal/gtk3"
-import { Variable } from "astal"
-import bar_style from "./widget/bar/style.scss"
-import Bar from "./widget/bar/Bar"
+import app from "ags/gtk4/app"
+import style from "./style.scss"
+import Bar from "./widget/Bar"
 
-import osd_style from "./widget/osd/OSD.scss"
-import OSD from "./widget/osd/OSD"
-
-import notification_style from "./widget/notifications/Notification.scss"
-import NotificationPopups from "./widget/notifications/NotificationPopups"
-
-import notification_panel_style from "./widget/notifications/Panel.scss"
-import NotificationPanel from "./widget/notifications/NotificationPanel"
-
-import PowerMenu from "./widget/powermenu/PowerMenu";
-import powermenu_style from "./widget/powermenu/PowerMenu.scss";
-
-import Alerts from "./widget/alerts/Alerts"
-import alerts_style from './widget/alerts/Alerts.scss'
-
-const show_popups = Variable(true)
-const show_panel = Variable(false)
-const bars: Widget.Window[] = []
-
-App.start({
-  css: bar_style,
+app.start({
+  css: style,
   main() {
-    App.get_monitors().map(
-      monitor =>
-        bars.push(Bar(monitor, show_popups) as Widget.Window)
-    )
+    app.get_monitors().map(Bar)
   },
-})
-
-App.start({
-  css: osd_style,
-  main() {
-    App.get_monitors().map(OSD)
-  },
-})
-
-App.start({
-  css: notification_style,
-  main() {
-    App.get_monitors().map(
-      monitor => NotificationPopups(monitor, show_popups)
-    )
-  }
-})
-
-App.start({
-  css: notification_panel_style,
-  main() {
-    App.get_monitors().map(
-      (monitor, i) =>
-        NotificationPanel(monitor, show_panel, show_popups, bars[i])
-    )
-  }
-})
-
-App.start({
-  css: powermenu_style,
-  main() {
-    App.get_monitors().map(PowerMenu)
-  },
-})
-
-App.start({
-  css: alerts_style,
-  main() {
-    App.get_monitors().map(Alerts)
-  }
 })
