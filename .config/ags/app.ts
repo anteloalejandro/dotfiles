ec796549c2db351b050cc4429f400e269a869abd
Alejandro Antelo <alfasant@protonmail.com>
2025-08-25 18:13:00 +0200

    bottom popup proof of concept

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -5,12 +5,15 @@ import BottomBar from "./widget/BottomBar"
 import LeftBar from "./widget/LeftBar"
 import RightBar from "./widget/RightBar"
 import { CornerWindow, CornerOrientation } from "./widget/corners"
+import { createState } from "gnim"
+
+const bottom_popup = createState(false);
 
 app.start({
   css: style,
   main() {
     app.get_monitors().map(TopBar)
-    app.get_monitors().map(BottomBar)
+    app.get_monitors().map(m => BottomBar(m, bottom_popup))
     app.get_monitors().map(LeftBar)
     app.get_monitors().map(RightBar)
 
@@ -20,5 +23,15 @@ app.start({
     app.get_monitors().map(m => CornerWindow(m, {orientation: TOP_RIGHT, radius}))
     app.get_monitors().map(m => CornerWindow(m, {orientation: BOTTOM_LEFT, radius}))
     app.get_monitors().map(m => CornerWindow(m, {orientation: BOTTOM_RIGHT, radius}))
+  },
+
+  requestHandler(req, res) {
+    switch (req) {
+      case "bottom_popup":
+        bottom_popup[1](b => !b);
+        res(bottom_popup[0].get());
+        break;
+      default: res("request not found");
+    }
   },
 })
--- .config/ags/widget/BottomBar.tsx
+++ .config/ags/widget/BottomBar.tsx
@@ -1,8 +1,11 @@
 import { Astal, Gdk, Gtk } from "ags/gtk4";
 import app from "ags/gtk4/app";
+import { timeout } from "ags/time";
+import { createState, onMount, State } from "gnim";
 
-export default function BottomBar(gdkmonitor: Gdk.Monitor) {
+export default function BottomBar(gdkmonitor: Gdk.Monitor, bottom_popup: State<boolean>) {
   const { BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
+  const [ popup_visible, set_popup_visible ] = bottom_popup;
   return (
     <window
       visible
@@ -13,16 +16,34 @@ export default function BottomBar(gdkmonitor: Gdk.Monitor) {
       anchor={BOTTOM | LEFT | RIGHT}
       application={app}
       height_request={8}
+      // keymode={Astal.Keymode.ON_DEMAND}
     >
-      <menubutton>
+      <menubutton sensitive={false}>
         <box class="border-bottom-bar"></box>
-          <popover
-            position={Gtk.PositionType.TOP}
-            autohide={false}
-            has_arrow={false}
+        <popover
+          $={self => {
+            // wait a lil bit before activating
+            onMount(() => timeout(250, () => {
+              self.popup();
+              // self.set_visible(true);
+            }));
+            // keep on trying until it works.
+            // self.connect("notify::visible", () => self.popup());
+          }}
+          position={Gtk.PositionType.TOP}
+          autohide={false}
+          has_arrow={false}
+        >
+          <revealer
+            transition_type={Gtk.RevealerTransitionType.SLIDE_UP}
+            reveal_child={popup_visible}
+            height_request={1}
           >
-            BOTTOM
-          </popover>
+            <box css="background-color: #181818; border-radius: 8px; color: white; padding: 10px;">
+              BOTTOM
+            </box>
+          </revealer>
+        </popover>
       </menubutton>
     </window>
   )
