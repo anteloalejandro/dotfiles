c0e538bccc26e1b9a832643dcac37b47b557eae2
Alejandro Antelo <alfasant@protonmail.com>
2025-09-02 12:29:14 +0200

    add sliders

--- .config/ags/widget/Resources.tsx
+++ .config/ags/widget/Resources.tsx
@@ -4,6 +4,7 @@ import { cpu_usage, mem, setup_fix_hidden_window, setup_window_resizable } from 
 import UiState from "../UiState";
 import { CornerOrientation, RoundedCorner } from "./corners";
 import { execAsync } from "ags/process";
+import Vars from "../Vars";
 
 export default function Resources(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT } = Astal.WindowAnchor;
@@ -34,13 +35,35 @@ export default function Resources(gdkmonitor: Gdk.Monitor) {
               class="resources-container"
               orientation={Gtk.Orientation.VERTICAL}
             >
-              <label label={cpu_usage.as(String)} />
-              <label label={mem.as(mem => `${mem.used}/${mem.total} MB`)} />
+              <box orientation={Gtk.Orientation.VERTICAL}>
+                <slider
+                  sensitive={false}
+                  value={cpu_usage}
+                  min={0} max={1}
+                />
+                <label label={cpu_usage.as(String)} />
+              </box>
+              <box orientation={Gtk.Orientation.VERTICAL}>
+                <slider
+                  sensitive={false}
+                  value={mem.as(m => m.used)}
+                  min={0} max={mem.as(m => m.total)}
+                />
+                <label label={mem.as(mem =>
+                  `${(mem.used/1000).toFixed(2)}/${(mem.total/1000).toFixed(2)} GB`
+                )} />
+              </box>
               <button
-                icon_name="process-stop-symbolic"
-                label="Kill"
-                onClicked={() => execAsync(["hyprctl", "kill"])}
-              />
+                onClicked={() => {
+                  execAsync(["hyprctl", "kill"]);
+                  set_reveal(false);
+                }}
+              >
+                <box spacing={Vars.spacing}>
+                  <image icon_name="process-stop-symbolic" />
+                  <label label="Kill" hexpand justify={Gtk.Justification.CENTER} />
+                </box>
+              </button>
             </box>
             <box valign={Gtk.Align.START}><RoundedCorner orientation={CornerOrientation.TOP_LEFT} /></box>
           </box>
