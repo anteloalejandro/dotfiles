bdb84dafcef306dcbb3c170a1908df9c58fc5db6
Alejandro Antelo <alfasant@protonmail.com>
2025-09-02 05:12:11 +0200

    basic styling in power menu

--- .config/ags/widget/PowerMenu.tsx
+++ .config/ags/widget/PowerMenu.tsx
@@ -1,9 +1,9 @@
 import { Astal, Gdk, Gtk } from "ags/gtk4";
 import app from "ags/gtk4/app";
 import { exec } from "ags/process";
-import { State } from "gnim";
 import UiState from "../UiState";
 import { setup_hide_on_escape } from "./utils";
+import Vars from "../Vars";
 
 export default function PowerMenu(gdkmonitor: Gdk.Monitor) {
   const [ visible, set_visible ] = UiState.show_power;
@@ -14,7 +14,7 @@ export default function PowerMenu(gdkmonitor: Gdk.Monitor) {
       name="power-menu"
       class="PowerMenu"
       gdkmonitor={gdkmonitor}
-      exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      exclusivity={Astal.Exclusivity.IGNORE}
       layer={Astal.Layer.OVERLAY}
       keymode={Astal.Keymode.EXCLUSIVE}
       anchor={BOTTOM | TOP | LEFT | RIGHT}
@@ -22,44 +22,47 @@ export default function PowerMenu(gdkmonitor: Gdk.Monitor) {
         setup_hide_on_escape(self, set_visible);
       }}
     >
-      <box valign={Gtk.Align.CENTER} halign={Gtk.Align.CENTER}>
-        <button
-          $={self => {
-            visible.subscribe(() => {
-              if (visible.get()) self.grab_focus();
-            })
-          }}
-          icon_name="system-shutdown-symbolic"
-          onClicked={() => {
-            exec("poweroff");
-          }}
-        />
-        <button
-          icon_name="system-reboot-symbolic"
-          onClicked={() => {
-            exec("reboot");
-          }}
-        />
-        <button
-          icon_name="system-suspend-symbolic"
-          onClicked={() => {
-            set_visible(false);
-            exec(["systemctl", "suspend"]);
-          }}
-        />
-        <button
-          icon_name="application-exit-symbolic"
-          onClicked={() => {
-            exec(["hyprctl", "dispatch", "exit"]);
-          }}
-        />
-        <button
-          icon_name="window-close-symbolic"
-          onClicked={() => {
-            set_visible(false);
-          }}
-        />
-      </box>
+      <overlay>
+        <box class="background" hexpand vexpand />
+        <box $type="overlay" valign={Gtk.Align.CENTER} halign={Gtk.Align.CENTER}>
+          <button
+            $={self => {
+              visible.subscribe(() => {
+                if (visible.get()) self.grab_focus();
+              })
+            }}
+            icon_name="system-shutdown-symbolic"
+            onClicked={() => {
+              exec("poweroff");
+            }}
+          />
+          <button
+            icon_name="system-reboot-symbolic"
+            onClicked={() => {
+              exec("reboot");
+            }}
+          />
+          <button
+            icon_name="system-suspend-symbolic"
+            onClicked={() => {
+              set_visible(false);
+              exec(["systemctl", "suspend"]);
+            }}
+          />
+          <button
+            icon_name="application-exit-symbolic"
+            onClicked={() => {
+              exec(["hyprctl", "dispatch", "exit"]);
+            }}
+          />
+          <button
+            icon_name="window-close-symbolic"
+            onClicked={() => {
+              set_visible(false);
+            }}
+          />
+        </box>
+      </overlay>
     </window>
   )
 }
