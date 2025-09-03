320ac7b29550d22783f87a309c1946582b1508c1
Alejandro Antelo <alfasant@protonmail.com>
2025-09-03 10:02:54 +0200

    fix Resources component's performance

--- .config/ags/widget/Resources.tsx
+++ .config/ags/widget/Resources.tsx
@@ -1,6 +1,6 @@
 import { Astal, Gdk, Gtk } from "ags/gtk4";
 import app from "ags/gtk4/app";
-import { cpu_usage, mem, setup_fix_hidden_window, setup_window_resizable } from "./utils";
+import { cpu_usage, mem } from "./utils";
 import UiState from "../UiState";
 import { CornerOrientation, RoundedCorner } from "./corners";
 import { execAsync } from "ags/process";
@@ -12,68 +12,59 @@ export default function Resources(gdkmonitor: Gdk.Monitor) {
 
   return (
     <window
-      visible 
-      application={app}
-      gdkmonitor={gdkmonitor}
+      visible={reveal}
+      namespace="resources"
       name="resources"
       class="Resources"
+      application={app}
+      gdkmonitor={gdkmonitor}
       layer={Astal.Layer.TOP}
       exclusivity={Astal.Exclusivity.NORMAL}
       anchor={TOP | LEFT}
-      $={self => {
-        setup_window_resizable(self, reveal, Gtk.Orientation.VERTICAL);
-        setup_fix_hidden_window(self, reveal);
-      }}
     >
-      <revealer
-        reveal_child={reveal}
-        transition_type={Gtk.RevealerTransitionType.SLIDE_DOWN}
-        height_request={1}
-      >
-        <box>
-          <box orientation={Gtk.Orientation.VERTICAL}>
-            <box
-              class="resources-container"
-              orientation={Gtk.Orientation.VERTICAL}
-              spacing={Vars.spacing}
+      <box>
+        <box orientation={Gtk.Orientation.VERTICAL}>
+          <box
+            class="resources-container"
+            orientation={Gtk.Orientation.VERTICAL}
+            spacing={Vars.spacing}
+          >
+            <box spacing={Vars.spacing}>
+              <image icon_name="memory-symbolic" />
+              <levelbar
+                orientation={Gtk.Orientation.HORIZONTAL}
+                width_request={200}
+                value={mem.as(m => m.used/m.total)}
+              >
+                <label label={mem.as(mem => `${(mem.used/1000).toFixed(2)}GB`)} />
+              </levelbar>
+            </box>
+            <box spacing={Vars.spacing}>
+              <image icon_name="cpu-symbolic" />
+              <levelbar
+                orientation={Gtk.Orientation.HORIZONTAL}
+                width_request={200}
+                value={cpu_usage(p => (p/100))}
+              >
+                <label label={cpu_usage.as(p => `${p}%`)} />
+              </levelbar>
+            </box>
+            <button
+              onClicked={() => {
+                execAsync(["hyprctl", "kill"]);
+                set_reveal(false);
+              }}
             >
               <box spacing={Vars.spacing}>
-                <image icon_name="memory-symbolic" />
-                <levelbar
-                  orientation={Gtk.Orientation.HORIZONTAL}
-                  width_request={200}
-                  value={mem.as(m => m.used/m.total)}
-                >
-                  <label label={mem.as(mem => `${(mem.used/1000).toFixed(2)}GB`)} />
-                </levelbar>
+                <image icon_name="process-stop-symbolic" />
+                <label label="Kill" hexpand justify={Gtk.Justification.CENTER} />
               </box>
-              <box spacing={Vars.spacing}>
-                <image icon_name="cpu-symbolic" />
-                <levelbar
-                  orientation={Gtk.Orientation.HORIZONTAL}
-                  width_request={200}
-                  value={cpu_usage(p => (p/100))}
-                >
-                  <label label={cpu_usage.as(p => `${p}%`)} />
-                </levelbar>
-              </box>
-              <button
-                onClicked={() => {
-                  execAsync(["hyprctl", "kill"]);
-                  set_reveal(false);
-                }}
-              >
-                <box spacing={Vars.spacing}>
-                  <image icon_name="process-stop-symbolic" />
-                  <label label="Kill" hexpand justify={Gtk.Justification.CENTER} />
-                </box>
-              </button>
-            </box>
-            <box valign={Gtk.Align.START}><RoundedCorner orientation={CornerOrientation.TOP_LEFT} /></box>
+            </button>
           </box>
-          <box halign={Gtk.Align.START}><RoundedCorner orientation={CornerOrientation.TOP_LEFT} /></box>
+          <box valign={Gtk.Align.START}><RoundedCorner orientation={CornerOrientation.TOP_LEFT} /></box>
         </box>
-      </revealer>
+        <box halign={Gtk.Align.START}><RoundedCorner orientation={CornerOrientation.TOP_LEFT} /></box>
+      </box>
     </window>
   )
 }
